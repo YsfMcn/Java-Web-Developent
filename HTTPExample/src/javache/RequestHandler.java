80e613ab8376103d209768900b7fcedb78111434
Jzph01 <jzph01@gmail.com>
2018-02-03 14:19:41 +0200

    HTTP-Server

---
+++ HTTPExample/src/javache/RequestHandler.java
@@ -0,0 +1,28 @@
+package stateManagement.src.javache;
+
+import javache.http.*;
+import db.entity.*;
+import stateManagement.src.javache.http.HttpRequest;
+import stateManagement.src.javache.http.HttpResponse;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class RequestHandler {
+    private HttpRequest httpRequest;
+
+    private HttpResponse httpResponse;
+
+    public RequestHandler(HttpSession session) {
+        this.session = session;
+    }
+
+    public byte[] handleRequest(String requestContent) {
+        
+    }
+}
---
+++ HTTPExample/src/javache/http/HttpSession.java
@@ -0,0 +1,11 @@
+package stateManagement.src.javache.http;
+
+import java.util.Map;
+
+public interface HttpSession {
+
+    void setSessionData(String sessionId, Map<String, Object> dataMap);
+
+    Map<String, Object> getSessionData(String sessionId);
+
+}
